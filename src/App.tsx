67f2cf6bee968cff5717f351de3883b581f3e2d9
Shinia-Gupta <shiniavit@gmail.com>
2025-10-29 16:40:24 +0530

    refetch data from the query on  button click

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   // The first property is a query key which is going to be an array. It is used for refetching data and caching data. It needs to be unique so that we know which query we are referencing. If it is not unique, lets say another query uses the exact same key, then queryclient will bot be able to distinguish between the 2 of them
 // The second property of this useQuery is a query Function denoted as queryFn. This is the function that will run whenever we run the query with this key. This is the function where we would put our API call 
 
-const {data, isPending}=useQuery({
+const {data, isPending,refetch}=useQuery({
   queryKey:['todos'],
   queryFn:getTodos
 
@@ -17,6 +17,9 @@ const {data, isPending}=useQuery({
     <>
     
     <div>{isPending?"Loading data":JSON.stringify(data.slice(0,10))}</div>
+
+    {/* Refresh the query whenever we click it */}
+    <button onClick={()=>refetch()}>Refetch</button>
     </>
   )
 }
